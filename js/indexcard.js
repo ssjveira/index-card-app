531f08b660a3e242fd7300a02163dfb574339f07
Robert Veira <robert.veira@gmail.com>
2022-09-13 02:43:32 -0400

    Updated index card app code to place all management of switching the "cards" into one class, "SightWordCardManager".

--- js/indexcard.js
+++ js/indexcard.js
@@ -1,42 +1,104 @@
-window.onload = function () {
-    var playing = false;
-    const words = ["all", "am", "are", "at", "ate", "be"];
+'use strict'
 
-    // Add cards to page
-    let cardContainer = document.getElementById("card-container");
-    words.forEach((word) => {
-        let frontElement = document.createElement("DIV");
-        frontElement.classList.add("front");
-        frontElement.appendChild(document.createTextNode(word));
+// Constants
+const animationDuration = 400;
+const words = ["all", "am", "are", "at", "ate", "be"];
 
-        let backElement = document.createElement("DIV");
-        backElement.classList.add("back");
-        backElement.appendChild(document.createTextNode(word));
+class SightWordCardManager
+{
+    #isAnimating = false;
+    #currentIndex = -1;
+    #cardContainerDiv = document.getElementById("card-container");
+    #cardDiv = document.getElementById('card');
+    #mainTitleDiv = document.getElementById('main-title');
+    #countTextDiv = document.getElementById('card-count');
+    #wordArray = []
 
-        let wordElement = document.createElement("DIV");
-        wordElement.classList.add("card");
-        wordElement.appendChild(frontElement);
-        wordElement.appendChild(backElement);
+    constructor(title, wordArray) {
+        this.#wordArray = wordArray;
 
-        cardContainer.appendChild(wordElement);
-    });
+        // Set main title
+        this.#mainTitleDiv.appendChild(document.createTextNode(title));
 
-    // Apply event listener to animate cards
-    var card = document.querySelector(".card");
-    card.addEventListener('click', function () {
-        if (playing)
-            return;
+        // Show first card
+        this.#cardContainerDiv.addEventListener('click', () => {
+            this.showNextCard();
+        });
+    }
 
-        playing = true;
+    doAnimation() {
+        let cardManager = this;
+        let cardDiv = this.#cardDiv;
         anime({
-            targets: card,
-            scale: [{ value: 1 }, { value: 1.4 }, { value: 1, delay: 250 }],
-            rotateY: { value: '+=180', delay: 200 },
+            targets: cardDiv,
+            opacity: '0',
             easing: 'easeInOutSine',
-            duration: 400,
-            complete: function (anim) {
-                playing = false;
+            duration: animationDuration,
+            complete: function (prevAnim) {
+                cardManager.updateText();
+                anime({
+                    targets: cardDiv,
+                    opacity: "1",
+                    easing: 'easeInOutSine',
+                    duration: animationDuration,
+                    complete: function (currAnim) {
+                        cardManager.setIsAnimating(false);
+                    }
+                });
             }
         });
-    });
+    }
+
+    // Taken from: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+    // Implementation of the Knuth shuffle
+    shuffle() {
+        let currentIndex = this.#wordArray.length,  randomIndex;
+
+        // While there remain elements to shuffle.
+        while (currentIndex != 0) {
+
+            // Pick a remaining element.
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+
+            // And swap it with the current element.
+            [this.#wordArray[currentIndex], this.#wordArray[randomIndex]] = [
+            this.#wordArray[randomIndex], this.#wordArray[currentIndex]];
+        }
+    }
+
+    showNextCard() {
+        if(this.#isAnimating) {
+            return;
+        }
+
+        this.#isAnimating = true;
+
+        if(this.#currentIndex == -1) {
+            this.#currentIndex = 0;
+        }
+        else if(this.#currentIndex == (this.#wordArray.length - 1)) { // Reshuffle and start back at the beginning
+            this.shuffle();
+            this.#currentIndex = 0;
+        }
+        else {
+            this.#currentIndex += 1;
+        }
+
+        this.doAnimation();
+    }
+
+    setIsAnimating(isAnimating) {
+        this.#isAnimating = isAnimating;
+    }
+
+    updateText() {
+        this.#cardDiv.textContent = this.#wordArray[this.#currentIndex];
+        this.#countTextDiv.textContent = `Card Count: ${this.#currentIndex + 1} out of ${this.#wordArray.length}` 
+    }
+}
+
+window.onload = function () {
+    const cardManager = new SightWordCardManager("Sight Words", words);
+    cardManager.showNextCard();
 }
